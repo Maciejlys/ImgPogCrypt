2eab744f9c302917623bdc5cd469276061d44547
Maciej Łyszczarz <62946872+Maciejlys@users.noreply.github.com>
2021-02-13 14:21:39 +0100

    feat(#1): string-utils-and-tests

---
+++ ImgPogCrypt/StringUtil.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace ImgPogCrypt
+{
+    public class StringUtil
+    {
+        public static string[] SplitStringToSet(string s)
+        {
+            List<string> list = new List<string>();
+            StringBuilder temp = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                temp.Append(s[i]);
+                if (temp.Length == 3)
+                {
+                    list.Add(temp.ToString());
+                    temp.Clear();
+                }
+            }
+
+            if (temp.Length != 0)
+            {
+                while (temp.Length < 3)
+                {
+                    temp.Append('0');
+                }
+
+                list.Add(temp.ToString());
+            }
+
+            return list.ToArray();
+        }
+    }
+}
---
+++ ImgPogCryptTests/StringUtilTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using ImgPogCrypt;
+using Xunit;
+
+namespace ImgPogCryptTests
+{
+    public class StringUtilTests
+    {
+        [Fact]
+        public void string_split_size()
+        {
+            string test = "01110100011001010111001101110100";
+            string[] splitBySize = StringUtil.SplitStringToSet(test);
+            Assert.DoesNotContain(splitBySize.ToList(),
+                x => x.Length > 3);
+        }
+
+        [Fact]
+        public void string_split_first_set_of_bytes()
+        {
+            string test = "01110100011001010111001101110100";
+            string[] splitBySize = StringUtil.SplitStringToSet(test);
+            Assert.Equal("011", splitBySize[0]);
+        }
+
+        [Fact]
+        public void string_split_second_set_of_bytes()
+        {
+            string test = "01110100011001010111001101110100";
+            string[] splitBySize = StringUtil.SplitStringToSet(test);
+            Assert.Equal("101", splitBySize[1]);
+        }
+
+        [Fact]
+        public void string_split_last_set_of_bytes_length_3()
+        {
+            string test = "101";
+            string[] splitBySize = StringUtil.SplitStringToSet(test);
+            Assert.Equal("101", splitBySize[^1]);
+        }
+
+        [Fact]
+        public void string_split_last_set_of_bytes_length_4()
+        {
+            string test = "1011";
+            string[] splitBySize = StringUtil.SplitStringToSet(test);
+            Assert.Equal("100", splitBySize[^1]);
+        }
+
+        [Fact]
+        public void string_split_last_set_of_bytes_length_5()
+        {
+            string test = "10111";
+            string[] splitBySize = StringUtil.SplitStringToSet(test);
+            Assert.Equal("110", splitBySize[^1]);
+        }
+
+        [Fact]
+        public void string_split_last_set_of_bytes_length_6()
+        {
+            string test = "101101";
+            string[] splitBySize = StringUtil.SplitStringToSet(test);
+            Assert.Equal("101", splitBySize[^1]);
+        }
+
+        [Fact]
+        public void string_build_check()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            Assert.Equal(0, stringBuilder.Length);
+        }
+    }
+}
