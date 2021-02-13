a78ed02c6a6a0b5129a29c1fd658a37b31fcc7f3
Maciej Łyszczarz <62946872+Maciejlys@users.noreply.github.com>
2021-02-13 23:18:44 +0100

    feat(#5): encrypted-image-parser-init

---
+++ ImgPogCrypt/EncryptedImageParser.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using ImgPogCrypt.model;
+
+namespace ImgPogCrypt
+{
+    public class EncryptedImageParser
+    {
+        // public List<RgbDifference> Parse()
+        // {
+        //     
+        // }
+    }
+}
