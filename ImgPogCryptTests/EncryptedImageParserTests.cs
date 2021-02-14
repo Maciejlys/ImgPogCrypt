40a4e78be146a2a797fe3be1cf8ec0e81f132dcb
Maciej Łyszczarz <62946872+Maciejlys@users.noreply.github.com>
2021-02-14 11:06:19 +0100

    fix(#5): requested-parser-test-fixes

--- ImgPogCryptTests/EncryptedImageParserTests.cs
+++ ImgPogCryptTests/EncryptedImageParserTests.cs
@@ -8,12 +8,17 @@ namespace ImgPogCryptTests
 {
     public class EncryptedImageParserTests
     {
+        private EncryptedImageParser _encrypter = new EncryptedImageParser();
+
         [Fact]
         public void decrypting_image_1_x_3()
         {
-            Bitmap bitmap = new Bitmap(@"C:\Users\archp\Desktop\TestPng.png");
-            EncryptedImageParser encrypter = new EncryptedImageParser();
-            List<RgbDifference> rgbDifferences = encrypter.Parse(bitmap);
+            Bitmap bitmap = new Bitmap(3, 1);
+            bitmap.SetPixel(0,0, Color.FromArgb(0,0,0));
+            bitmap.SetPixel(1,0, Color.FromArgb(0,0,1));
+            bitmap.SetPixel(2,0, Color.FromArgb(1,0,1));
+            List<RgbDifference> rgbDifferences = _encrypter.Parse(bitmap);
+            
             RgbDifference expectedFirstPixel = new RgbDifference(0, 0, 0);
             RgbDifference expectedSecondPixel = new RgbDifference(0, 0, 1);
             RgbDifference expectedThirdPixel = new RgbDifference(1, 0, 1);
@@ -26,9 +31,12 @@ namespace ImgPogCryptTests
         [Fact]
         public void decrypting_image_2_x_2()
         {
-            Bitmap bitmap = new Bitmap(@"C:\Users\archp\Desktop\TestPng2.png");
-            EncryptedImageParser encrypter = new EncryptedImageParser();
-            List<RgbDifference> rgbDifferences = encrypter.Parse(bitmap);
+            Bitmap bitmap = new Bitmap(2, 2);
+            bitmap.SetPixel(0,0, Color.FromArgb(0,0,0));
+            bitmap.SetPixel(1,0, Color.FromArgb(0,1,1));
+            bitmap.SetPixel(0,1, Color.FromArgb(0,1,0));
+            bitmap.SetPixel(1,1, Color.FromArgb(1,1,1));
+            List<RgbDifference> rgbDifferences = _encrypter.Parse(bitmap);
             RgbDifference expectedFp = new RgbDifference(0, 0, 0);
             RgbDifference expectedSp = new RgbDifference(0, 1, 1);
             RgbDifference expectedSlFp = new RgbDifference(0, 1, 0);
