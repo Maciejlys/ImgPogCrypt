bc804bd8b11ef91119579a2211e18fab1610e16a
Maciej Łyszczarz <62946872+Maciejlys@users.noreply.github.com>
2021-02-14 13:02:37 +0100

    feat(#10): base-decryptor-and-test

---
+++ ImgPogCrypt/BaseDecryptor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ImgPogCrypt.lib;
+using ImgPogCrypt.model;
+
+namespace ImgPogCrypt
+{
+    public class BaseDecryptor : IDecryptor
+    {
+        public string Decrypt(List<RgbDifference> diff)
+        {
+            string diffs = ConvertRgbDiffListToString(diff);
+            return BinaryToString(diffs);
+        }
+
+        private string ConvertRgbDiffListToString(List<RgbDifference> rgbDifferences)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (RgbDifference rgbDifference in rgbDifferences)
+            {
+                sb.Append(ConvertRgbDiffToString(rgbDifference));
+            }
+
+            return sb.ToString();
+        }
+
+        private string ConvertRgbDiffToString(RgbDifference rgbDifference)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(rgbDifference.R);
+            sb.Append(rgbDifference.G);
+            sb.Append(rgbDifference.B);
+            return sb.ToString();
+        }
+        
+        public static string BinaryToString(string data)
+        {
+            List<Byte> byteList = new List<Byte>();
+ 
+            for (int i = 0; i < data.Length; i += 8)
+            {
+                byteList.Add(Convert.ToByte(data.Substring(i, 8), 2));
+            }
+            return Encoding.ASCII.GetString(byteList.ToArray());
+        }
+    }
+}
---
+++ ImgPogCryptTests/BaseDecrytptorTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using ImgPogCrypt;
+using ImgPogCrypt.lib;
+using ImgPogCrypt.model;
+using Xunit;
+
+namespace ImgPogCryptTests
+{
+    public class BaseDecrytptorTest
+    {
+        private IDecryptor _decryptor;
+
+        public BaseDecrytptorTest()
+        {
+            _decryptor = new BaseDecryptor();
+        }
+
+        [Fact]
+        public void binary_to_string()
+        {
+            string expected = "z";
+            List<RgbDifference> rgbDifferences = new List<RgbDifference>();
+            // 01111010
+            rgbDifferences.Add(new RgbDifference(0,1,1));
+            rgbDifferences.Add(new RgbDifference(1,0,1));
+            
+            Assert.Equal(expected, _decryptor.Decrypt(rgbDifferences));
+        }
+    }
+}
