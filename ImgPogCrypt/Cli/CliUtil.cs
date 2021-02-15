05f55dbd7d3ef2fbbfc075e10bca189272d0ac5f
Maciej Łyszczarz <62946872+Maciejlys@users.noreply.github.com>
2021-02-15 16:59:01 +0100

    Feat/#12 cli (#13)
    * feat(#12): cli
    
    * fix(#12): cli-fix

---
+++ ImgPogCrypt/Cli/CliUtil.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ImgPogCrypt
+{
+    public class CliUtil
+    {
+        public static List<CliCommand> ParseArgs(string[] args)
+        {
+            return args.ToList().Select(CliUtil.StringToCommand).ToList();
+        }
+
+        private static CliCommand StringToCommand(string arg)
+        {
+            CliAction cliAction;
+            string value = "";
+            if (arg.Contains("="))
+            {
+                string[] strings = arg.Split("=");
+                value = strings[1];
+                cliAction = StringToCliAction(strings[0]);
+            }
+            else
+            {
+                cliAction = StringToCliAction(arg);
+            }
+
+            return new CliCommand(cliAction, value);
+        }
+
+        private static CliAction StringToCliAction(string s)
+        {
+            switch (s)
+            {
+                default:
+                    throw new ArgumentException("Unsupported action");
+
+                case "-em":
+                    return CliAction.Em;
+                case "-o":
+                    return CliAction.O;
+                case "-d":
+                    return CliAction.D;
+                case "-ef":
+                    return CliAction.Ef;
+                case "-h":
+                    return CliAction.H;
+            }
+        }
+    }
+}
