13957ff9119e00fc07a9e2629c6a2fbf8f7c30f1
bartekkur1 <bartekkur1@gmail.com>
2021-02-17 21:22:00 +0100

    ImgPogCrypt single responsibility methods

--- ImgPogCrypt/ImgPogCrypt.cs
+++ ImgPogCrypt/ImgPogCrypt.cs
@@ -23,82 +23,106 @@ namespace ImgPogCrypt
 
         public void Start()
         {
-            List<CliCommand> cliCommands = CliUtil.ParseArgs(args);
-
-            CliCommand encryptCommand = cliCommands.Find(c => c.Action == CliAction.Em || c.Action == CliAction.Ef);
-            CliCommand decryptCommand = cliCommands.Find(c => c.Action == CliAction.D);
-            CliCommand baseMapCommand = cliCommands.Find(c => c.Action == CliAction.B);
-
-            if (encryptCommand != null)
+            try
             {
-                Bitmap outputMap = null;
-                CliCommand outputCommand = cliCommands.Find(c => c.Action == CliAction.O);
-                if (outputCommand == null) throw new Exception("Unsupported action, make sure to give path -o missing");
+                List<CliCommand> cliCommands = CliUtil.ParseArgs(args);
 
-                if (encryptCommand.Action == CliAction.Em)
+                CliCommand encryptCommand = cliCommands.Find(c => c.Action == CliAction.Em || c.Action == CliAction.Ef);
+                CliCommand decryptCommand = cliCommands.Find(c => c.Action == CliAction.D);
+                CliCommand baseMapCommand = cliCommands.Find(c => c.Action == CliAction.B);
+
+                if (encryptCommand != null)
                 {
-                    if (baseMapCommand != null && File.Exists(baseMapCommand.Value))
-                    {
-                        Bitmap inputMap = new Bitmap(baseMapCommand.Value);
-                        outputMap = Converter.ToBitmap(Encryptor.Encrypt(encryptCommand.Value), inputMap);
-                    }
-                    else
-                    {
-                        outputMap = Converter.ToBitmap(Encryptor.Encrypt(encryptCommand.Value));
-                    }
+                    CliCommand outputCommand = cliCommands.Find(c => c.Action == CliAction.O);
+                    if (outputCommand == null) throw new Exception("Unsupported action, make sure to give path -o missing");
+
+                    EncryptImage(encryptCommand, baseMapCommand, outputCommand.Value);
                 }
-                else if (encryptCommand.Action == CliAction.Ef && File.Exists(encryptCommand.Value))
+                else if (decryptCommand != null && File.Exists(decryptCommand.Value))
                 {
-                    String inputFile = File.ReadAllText(encryptCommand.Value, Encoding.UTF8);
-
-                    if (baseMapCommand != null && File.Exists(baseMapCommand.Value))
-                    {
-                        Bitmap inputMap = new Bitmap(baseMapCommand.Value);
-                        outputMap = Converter.ToBitmap(Encryptor.Encrypt(inputFile), inputMap);
-                    }
-                    else
-                    {
-                        outputMap = Converter.ToBitmap(Encryptor.Encrypt(inputFile));
-                    }
+                    DecryptImage(decryptCommand, baseMapCommand);
                 }
-
-                if (outputMap != null)
+                else if (cliCommands.Any(c => c.Action == CliAction.H))
                 {
-                    outputMap.Save(outputCommand.Value);
+                    PrintHelpDialog();
                 }
                 else
                 {
-                    throw new ArgumentException("Invalid arguments");
+                    throw new Exception("Unsupported action, use -d or -e");
                 }
             }
-            else if (decryptCommand != null && File.Exists(decryptCommand.Value))
+            catch (Exception exception)
             {
-                Bitmap inputMap = new Bitmap(decryptCommand.Value);
-                String result = "";
-                if (baseMapCommand != null && File.Exists(baseMapCommand.Value))
-                {
-                    Bitmap basemap = new Bitmap(baseMapCommand.Value);
-                    result = Decryptor.Decrypt(Parser.Parse(inputMap, basemap));
-                }
-                else
-                {
-                    result = Decryptor.Decrypt(Parser.Parse(inputMap));
-                }
+                Console.WriteLine("A wild error has occured!");
+                Console.WriteLine(exception.Message);
+            }
+        }
 
-                Console.WriteLine(result);
-            }
-            else if (cliCommands.Any(c => c.Action == CliAction.H))
+        private void PrintHelpDialog()
+        {
+            Console.WriteLine("-em : secret message to encrypt into image");
+            Console.WriteLine("-o : output where  image should be stored");
+            Console.WriteLine("-d : decrypt from image file path");
+            Console.WriteLine("-ef : path to a file with text to encrypt into image");
+            Console.WriteLine("-o must be used with -em or -ef");
+            Console.WriteLine("-b  : base image");
+        }
+
+        private void DecryptImage(CliCommand decryptCommand, CliCommand baseMapCommand)
+        {
+            Bitmap inputMap = new Bitmap(decryptCommand.Value);
+            String result = "";
+            if (baseMapCommand != null && File.Exists(baseMapCommand.Value))
             {
-                Console.WriteLine("-em : secret message to encrypt into image");
-                Console.WriteLine("-o : output where  image should be stored");
-                Console.WriteLine("-d : decrypt from image file path");
-                Console.WriteLine("-ef : path to a file with text to encrypt into image");
-                Console.WriteLine("-o must be used with -em or -ef");
-                Console.WriteLine("-b  : base image");
+                Bitmap basemap = new Bitmap(baseMapCommand.Value);
+                result = Decryptor.Decrypt(Parser.Parse(inputMap, basemap));
             }
             else
             {
-                throw new Exception("Unsupported action, use -d or -e");
+                result = Decryptor.Decrypt(Parser.Parse(inputMap));
+            }
+
+            Console.WriteLine(result);
+        }
+
+        private void EncryptImage(CliCommand encryptCommand, CliCommand baseMapCommand, String outputPath)
+        {
+            Bitmap outputMap = null;
+            
+            if (encryptCommand.Action == CliAction.Em)
+            {
+                if (baseMapCommand != null && File.Exists(baseMapCommand.Value))
+                {
+                    Bitmap inputMap = new Bitmap(baseMapCommand.Value);
+                    outputMap = Converter.ToBitmap(Encryptor.Encrypt(encryptCommand.Value), inputMap);
+                }
+                else
+                {
+                    outputMap = Converter.ToBitmap(Encryptor.Encrypt(encryptCommand.Value));
+                }
+            }
+            else if (encryptCommand.Action == CliAction.Ef && File.Exists(encryptCommand.Value))
+            {
+                String inputFile = File.ReadAllText(encryptCommand.Value, Encoding.UTF8);
+
+                if (baseMapCommand != null && File.Exists(baseMapCommand.Value))
+                {
+                    Bitmap inputMap = new Bitmap(baseMapCommand.Value);
+                    outputMap = Converter.ToBitmap(Encryptor.Encrypt(inputFile), inputMap);
+                }
+                else
+                {
+                    outputMap = Converter.ToBitmap(Encryptor.Encrypt(inputFile));
+                }
+            }
+
+            if (outputMap != null)
+            {
+                outputMap.Save(outputPath);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid arguments");
             }
         }
     }
